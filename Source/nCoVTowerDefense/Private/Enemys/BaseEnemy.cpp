9e8fc1116245d20af7c018feb248fabeb4194379
Druid <DruidEdu@163.com>
2020-02-06 00:09:37 +0800

    chore:修改敌人父类为character

--- Source/nCoVTowerDefense/Private/Enemys/BaseEnemy.cpp
+++ Source/nCoVTowerDefense/Private/Enemys/BaseEnemy.cpp
@@ -1,12 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Public/Enemys/BaseEnemy.h"
+#include "BaseEnemy.h"
 
 // Sets default values
 ABaseEnemy::ABaseEnemy()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 }
@@ -22,6 +22,13 @@ void ABaseEnemy::BeginPlay()
 void ABaseEnemy::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABaseEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 }
 
--- Source/nCoVTowerDefense/Public/Enemys/BaseEnemy.h
+++ Source/nCoVTowerDefense/Public/Enemys/BaseEnemy.h
@@ -3,16 +3,16 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
+#include "GameFramework/Character.h"
 #include "BaseEnemy.generated.h"
 
 UCLASS()
-class NCOVTOWERDEFENSE_API ABaseEnemy : public AActor
+class NCOVTOWERDEFENSE_API ABaseEnemy : public ACharacter
 {
 	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
+
+public:
+	// Sets default values for this character's properties
 	ABaseEnemy();
 
 protected:
@@ -22,5 +22,8 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 };
